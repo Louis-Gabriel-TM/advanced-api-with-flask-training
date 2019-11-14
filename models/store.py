cf6568c345f5358808e0686d08888ef64be54c8e
loic-mangin <loic.mangin@naxos.fr>
2019-11-14 16:46:09 +0100

    Model: add 'unique=True' to the 'name' column of ItemModel and StoreModel

--- models/store.py
+++ models/store.py
@@ -6,7 +6,7 @@ class StoreModel(db.Model):
     __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(80), unique=True)
 
     items = db.relationship('ItemModel', lazy='dynamic')
     # 'dynamic' indicates the items of a store can be query, 
